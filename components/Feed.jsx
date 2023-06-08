c10d6d8ec9a440868fad76a2b18d0845de2fe5c8
hassanziya <60289732+hassanziya@users.noreply.github.com>
2023-06-08 23:16:42 +0530

    solving-cache-issue-3

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -23,7 +23,7 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch('/api/prompt', {
+      const response = await fetch('/api/prompt/all', {
         method: 'GET',
         headers: {
           'Cache-Control': 'no-store, no-cache, must-revalidate',
