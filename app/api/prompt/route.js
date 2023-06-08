dbe5ac41684cb9acec2dc60ef65b76a1596f76c3
hassanziya <60289732+hassanziya@users.noreply.github.com>
2023-06-08 23:07:46 +0530

    solving-cache-issue

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -6,8 +6,13 @@ export const GET = async (req) => {
     await connectToDB();
 
     const prompts = await Prompt.find({}).populate('creator');
-
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    const headers = {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-store, no-cache, must-revalidate',
+      Expires: '0',
+      Pragma: 'no-cache',
+    };
+    return new Response(JSON.stringify(prompts), { status: 200, headers });
   } catch (error) {
     return new Response('Failed to fetch all prompts', { status: 500 });
   }
