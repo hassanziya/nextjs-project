a0f7c4e4f17aea621257bd308a9d70c0f9bd9edd
hassanziya <60289732+hassanziya@users.noreply.github.com>
2023-06-09 23:01:09 +0530

    session-issue-3

--- app/profile/page.js
+++ app/profile/page.js
@@ -17,7 +17,7 @@ const MyProfile = () => {
     };
 
     if (session?.user.id) fetchPosts();
-  }, []);
+  }, [session]);
   const handleEdit = (post) => {
     router.push(`/update-prompt?id=${post._id}`);
   };
